63121dafd7b4c24f83dc4e1b687006cd8ed4303e
mittalsarthak <mittalsarthak24@gmail.com>
2020-11-15 22:14:01 +0530

    added a step to extract product info from csv file

--- src/main/java/com/learn/sbb/config/BatchConfiguration.java
+++ src/main/java/com/learn/sbb/config/BatchConfiguration.java
@@ -1,9 +1,9 @@
 package com.learn.sbb.config;
 
-import com.learn.sbb.SbbApplication;
 import com.learn.sbb.listener.HwJobExecutionListener;
 import com.learn.sbb.listener.HwStepExecutionListener;
 import com.learn.sbb.listener.Step2Listener;
+import com.learn.sbb.model.Product;
 import com.learn.sbb.processor.InMemoryItemProcessor;
 import com.learn.sbb.reader.InMemoryReader;
 import com.learn.sbb.writer.ConsoleItemWriter;
@@ -16,13 +16,20 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.mapping.FieldSetMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.PathResource;
+import org.springframework.core.io.UrlResource;
 
-import java.io.Console;
+import java.net.MalformedURLException;
 
 @EnableBatchProcessing
 @Configuration
@@ -61,6 +68,34 @@ public class BatchConfiguration {
         };
     }
 
+    @Bean
+    public FlatFileItemReader flatFileItemReader() {
+        FlatFileItemReader reader = new FlatFileItemReader();
+
+        //Step 1: Let the reader know where is the file
+        reader.setResource(new FileSystemResource("input/products.csv"));
+
+        //Step 2: create the line mapper
+        reader.setLineMapper(
+                new DefaultLineMapper(){
+                    {
+                        setLineTokenizer(new DelimitedLineTokenizer(){
+                            {
+                                setNames(new String[] {"productId","productName","productDesc","price","unit"});
+                            }
+                        });
+                        setFieldSetMapper(new BeanWrapperFieldSetMapper<Product>(){
+                            {
+                                setTargetType(Product.class);
+                            }
+                        });
+                    }
+                }
+        );
+        //Step 3: Tell reader to skip the header
+        reader.setLinesToSkip(1);
+        return reader;
+    }
 
     @Bean
     public Step step1() {
@@ -70,6 +105,7 @@ public class BatchConfiguration {
                 .build();
     }
 
+    @Bean
     public Step step2() {
         return steps.get("step2")
                 .listener(step2Listener).
@@ -81,12 +117,22 @@ public class BatchConfiguration {
     }
 
     @Bean
+    public Step step3() {
+        return steps.get("step3")
+                .<String, String>chunk(1)
+                .reader(flatFileItemReader())
+                .writer(consoleItemWriter)
+                .build();
+    }
+
+    @Bean
     public Job helloWorldJob() {
         return jobs.get("helloWorldJob")
                 .incrementer(new RunIdIncrementer())
                 .listener(hwJobExecutionListener)
                 .start(step1())
                 .next(step2())
+                .next(step3())
                 .build();
     }
 }
